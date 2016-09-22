bd91776b84c97dd2888f4b5227210927d4d40473
CS User <csuser@pangolin.(none)>
2016-09-22 07:22:43 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cosc440asign2
+
+To run assignment2.py:
+	sudo python assignement2.py [username] [shadow file]
+
+-sudo is used so that you have permission to open the shadow file
+-python is used to be able to compile and run assignment2.txt
+-assignment2.py is the name of the file to be ran
+-[username] is telling the program what username to look for in the shadow file
+-[shadow file] is you expicitly telling the program where your shadow file is located
--- assignment2.py
+++ assignment2.py
@@ -3,27 +3,26 @@ import crypt
 
 def main():
 	str = ""
-	for line in open(sys.argv[2], "r").readlines():
+	for line in open(sys.argv[2], "r").readlines(): #open file and iterate through it
 		if sys.argv[1] in line: #checks for the username in the line
 	 		str = line
-	if str == "":
+	if str == "": #if the username is not found
 	 	print "Username does not exist",
 	 	sys.exit(0)
 
-	sections = str.split(":") #line in the shadow
-	parts = sections[1].split("$") #parts of the encryted pw
-	salt = "$" + parts[1] + "$" + parts[2] + "$"
-
+	sections = str.split(":") #line in the shadow split apart
+	parts = sections[1].split("$") #parts of the encryted pw split apart
+	salt = "$" + parts[1] + "$" + parts[2] + "$" #type of encryption & actual salt
 
 	pw = ""
-	with open("/usr/share/dict/american-english", "r") as f:
+	with open("/usr/share/dict/american-english", "r") as f: #iterate through dictionary
 		for line in f:
 			for word in line.split():
-				if crypt.crypt(word, salt) == sections[1]:
+				if crypt.crypt(word, salt) == sections[1]: #encrypt every word & test it
 					pw = word
 					print "Success! The password is: " + pw
 					sys.exit(0)
-	if pw == "":
+	if pw == "": 
 		print "Password is not in the directory"
 
 if __name__=="__main__":
